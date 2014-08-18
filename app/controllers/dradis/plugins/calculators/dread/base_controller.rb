28d378cc55e72a6013a53ae326f8b7ae51a4b37a
Daniel Martin <etd-github@nomejortu.com>
2014-08-18 16:07:16 +0200

    Controller - inherit from vanilla ActionController::Base
    This is because different editions of Dradis have a different base
    controller, not a great thing. And at the end of the day, we don't need
    any functionality, not even authentication.

--- app/controllers/dradis/plugins/calculators/dread/base_controller.rb
+++ app/controllers/dradis/plugins/calculators/dread/base_controller.rb
@@ -1,7 +1,5 @@
 module Dradis::Plugins::Calculators::DREAD
   # Does it matter that we're inheriting from the no-frills controller?
-  class BaseController < ApplicationController
-    def index
-    end
+  class BaseController < ActionController::Base
   end
 end
