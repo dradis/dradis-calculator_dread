148fa64bda205ac0ce463782512299a69136e22b
Sean Yeoh <sean.yeoh@hotmail.com>
2021-01-25 15:23:40 +0800

    Add tylium.js to precompile list

--- lib/dradis/plugins/calculators/dread/engine.rb
+++ lib/dradis/plugins/calculators/dread/engine.rb
@@ -9,6 +9,7 @@ module Dradis::Plugins::Calculators::DREAD
     initializer 'calculator_dread.asset_precompile_paths' do |app|
       app.config.assets.precompile += [
         'dradis/plugins/calculators/dread/manifests/application.js',
+        'dradis/plugins/calculators/dread/manifests/tylium.js',
         'dradis/plugins/calculators/dread/manifests/application.css',
       ]
     end
