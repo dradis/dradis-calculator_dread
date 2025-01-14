fd3ce6d6a6495602db8aea27cd698d79f3e86554
Caitlin <caitlinhenry.ss@gmail.com>
2025-01-14 10:37:07 -0500

    use`lease_connection` instead of soft-deprecated `connection`

--- lib/dradis/plugins/calculators/dread/engine.rb
+++ lib/dradis/plugins/calculators/dread/engine.rb
@@ -25,12 +25,14 @@ module Dradis::Plugins::Calculators::DREAD
       # initialization, we first check if the DB is loaded and the Configuration
       # table has been created, before checking if the engine is enabled
       Rails.application.reloader.to_prepare do
-        if (ActiveRecord::Base.connection rescue false) && ::Configuration.table_exists?
-          Rails.application.routes.append do
-            # Enabling/disabling integrations calls Rails.application.reload_routes! we need the enable
-            # check inside the block to ensure the routes can be re-enabled without a server restart
-            if Engine.enabled?
-              mount Engine => '/', as: :dread_calculator
+        ActiveRecord::Base.lease_connection do
+          if ::Configuration.table_exists?
+            Rails.application.routes.append do
+              # Enabling/disabling integrations calls Rails.application.reload_routes! we need the enable
+              # check inside the block to ensure the routes can be re-enabled without a server restart
+              if Engine.enabled?
+                mount Engine => '/', as: :dread_calculator
+              end
             end
           end
         end
