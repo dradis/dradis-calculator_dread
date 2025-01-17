ba78f9630042a56ecfef165ca60319afa1f6f698
Caitlin <caitlinhenry.ss@gmail.com>
2025-01-17 12:45:07 -0500

    remove outdated comment

--- lib/dradis/plugins/calculators/dread/engine.rb
+++ lib/dradis/plugins/calculators/dread/engine.rb
@@ -21,10 +21,6 @@ module Dradis::Plugins::Calculators::DREAD
     end
 
     initializer 'calculator_dread.mount_engine' do
-      # By default, this engine is loaded into the main app. So, upon app
-      # initialization, we first check if the DB is loaded and the Configuration
-      # table has been created, before checking if the engine is enabled
-      # Rails.application.reloader.to_prepare do
         Rails.application.routes.append do
           # Enabling/disabling integrations calls Rails.application.reload_routes! we need the enable
           # check inside the block to ensure the routes can be re-enabled without a server restart
